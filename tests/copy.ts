0572a0b9a768fe44dfc02275bf2603f1ac74a9ac
Kyle Upton <kyleaupton@gmail.com>
2023-10-07 12:36:24 -0400

    wip - switching computers

--- tests/copy.ts
+++ tests/copy.ts
@@ -1,21 +1,19 @@
 import { setPath } from '../src/bin'
 import { copy } from '../src/copy'
 
-(async () => {
-  setPath('/opt/homebrew/Cellar/rsync/3.2.7_1/bin/rsync')
+setPath('/opt/homebrew/Cellar/rsync/3.2.7_1/bin/rsync')
 
-  const res = await copy({
-    source: '/Users/kyleupton/Downloads/',
-    destination: '/Users/kyleupton/Documents/dest/',
-    options: {
-      archive: true,
-      include: '*.mkv',
-      exclude: '*'
-    },
-    onProgress: (progress) => {
-      console.log(progress)
-    }
-  })
+const res = await copy({
+  source: '/Users/kyleupton/Downloads/',
+  destination: '/Users/kyleupton/Documents/dest/',
+  options: {
+    archive: true,
+    include: '*.iso',
+    exclude: '*'
+  },
+  onProgress: (progress) => {
+    console.log(progress)
+  }
+})
 
-  console.log(res)
-})()
+console.log(res)
