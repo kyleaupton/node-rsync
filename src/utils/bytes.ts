9c4cf1e47b5077cca1caf1273204b6495f32f642
Kyle Upton <kyleaupton@gmail.com>
2023-10-07 14:54:01 -0400

    formatting

--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -1,5 +1,5 @@
-export const humanReadableToBytes = (string: string) => {
-  const units: { [key: string]: number } = {
+export const humanReadableToBytes = (string: string): { bits: number, bytes: number } => {
+  const units: Record<string, number> = {
     kB: 1e+3,
     MB: 1e+6,
     GB: 1e+9,
@@ -7,28 +7,28 @@ export const humanReadableToBytes = (string: string) => {
     PB: 1e+15,
     EB: 1e+18,
     ZB: 1e+21,
-    YB: 1e+24,
+    YB: 1e+24
   }
 
-  const match = string.match(/(?<n>[\d\.]+)(?<unit>\w{2})/)
+  const match = string.match(/(?<n>[\d\.]+)(?<unit>\w{2})/) // eslint-disable-line
 
-  if (match && match.groups) {
+  if (match?.groups) {
     const { n, unit } = match.groups
 
-    const factor = units[unit];
+    const factor = units[unit]
 
     if (factor) {
-      const bytes = +n * factor;
+      const bytes = +n * factor
 
       return {
         bits: bytes * 8,
-        bytes,
+        bytes
       }
     }
   }
 
   return {
     bits: 0,
-    bytes: 0,
+    bytes: 0
   }
 }
